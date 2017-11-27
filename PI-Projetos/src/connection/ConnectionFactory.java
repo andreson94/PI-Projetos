9d407e36e97ac15f69d6bdc7df792beb7b069ad7
joao.vrevangelista <joao.vrevangelista@CAS555590W0737.senacsp.edu.br>
2017-11-27 19:28:59 -0200

    alteração

--- PI-Projetos/src/connection/ConnectionFactory.java
+++ PI-Projetos/src/connection/ConnectionFactory.java
@@ -5,11 +5,7 @@
  */
 package connection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 /**
  *
@@ -17,30 +13,8 @@ import java.util.logging.Logger;
  */
 public class ConnectionFactory {
     
-    private final String DRIVER = "com.mysql.jdbc.Driver";
-    private final String URL = "jdbc:mysql://localhost:3306/dbcalcados";
-    private final String USER = "root";
-    private final String PASS = "";
     
-    public Connection getConnection(){
-        try {
-            Class.forName(DRIVER);
-            return DriverManager.getConnection(URL, USER, PASS);
-        } catch (ClassNotFoundException | SQLException ex) {
-            throw new RuntimeException("Erro na conexão: ", ex);
-        }
-       
-    }
-    public static void closeConnection(Connection con){
-        
-            try {
-                if(con!=null){
-                con.close();
-                }
-            } catch (SQLException ex) {
-                Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+    
     }
 
 
