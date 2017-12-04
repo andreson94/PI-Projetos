9319d6405994afd18c1cfac5af650a80714560a3
joao.vrevangelista <joao.vrevangelista@CAS555589W0736.senacsp.edu.br>
2017-12-04 21:41:05 -0200

    vito gay

--- PI-Projetos/src/model/dao/DaoProdutos.java
+++ PI-Projetos/src/model/dao/DaoProdutos.java
@@ -54,7 +54,7 @@ public class DaoProdutos {
             throws SQLException, Exception {
         
         String sql = "UPDATE produto SET modelo=?, tamanho=?, marca=?, cor=?, genero=?, preco=? "
-                + "WHERE (produto_id=?)";
+                + "WHERE (codproduto=?)";
        
         Connection connection = null;
        
@@ -109,7 +109,7 @@ public class DaoProdutos {
             if (preparedStatement != null && !preparedStatement.isClosed()) {
                 preparedStatement.close();
             }
-            //Se a conexão ainda estiver aberta, realiza seu fechamento
+           
             if (connection != null && !connection.isClosed()) {
                 connection.close();
             }
@@ -143,7 +143,7 @@ public class DaoProdutos {
                 if (listaProdutos == null) {
                     listaProdutos = new ArrayList<Produto>();
                 }
-                //Cria uma instância de Quarto e popula com os valores do BD
+                
                 Produto produto = new Produto();
                 produto.setCod_produto(result.getLong("codproduto"));
                 produto.setModelo(result.getString("modelo"));
@@ -152,90 +152,146 @@ public class DaoProdutos {
                 produto.setCor(result.getString("cor"));
                 produto.setGenero(result.getString("genero"));
                 produto.setPreco(result.getFloat("preco"));
-                produto.setSituacao(result.getString("situacao"));
+               
                 
-                //Adiciona a instância na lista
+                
                 listaProdutos.add(produto);
             }
         } finally {
-            //Se o result ainda estiver aberto, realiza seu fechamento
+            
             if (result != null && !result.isClosed()) {
                 result.close();
             }
-            //Se o preparedStatement ainda estiver aberto, realiza seu fechamento
+           
             if (preparedStatement != null && !preparedStatement.isClosed()) {
                 preparedStatement.close();
             }
-            //Se a conexão ainda estiver aberta, realiza seu fechamento
+            
             if (connection != null && !connection.isClosed()) {
                 connection.close();
             }
         }
-        //Retorna a lista de quartos do banco de dados
+        
         return listaProdutos;
     }
       public static List<Produto> procurar(Long valor)
             throws SQLException, Exception {
-        //Monta a string de consulta de quartos no banco, utilizando
-        //o valor passado como parâmetro para busca na coluna de
-        //numero. Além disso, também considera apenas os elementos
-        //que possuem a coluna de ativação de quartos configurada com
-        //o valor correto ("enabled" com "true")
-        String sql = "SELECT * FROM quarto WHERE (numero=? AND enabled=?)";
+     
+        String sql = "SELECT * FROM produto WHERE (modelo=? AND enabled=?)";
 
-        //Lista de quartos de resultado
-        List<Quarto> listaQuartos = null;
-        //Conexão para abertura e fechamento
+       
+        List<Produto> listaProduto = null;
+        
         Connection connection = null;
-        //Statement para obtenção através da conexão, execução de
-        //comandos SQL e fechamentos
+       
         PreparedStatement preparedStatement = null;
-        //Armazenará os resultados do banco de dados
+        
         ResultSet result = null;
         try {
-            //Abre uma conexão com o banco de dados
+           
             connection = ConnectionUtils.getConnection();
-            //Cria um preparedStatement para execução de instruções SQL
+           
             preparedStatement = connection.prepareStatement(sql);
-            //Configura os parâmetros do PreparedStatement
+            
             preparedStatement.setLong(1, valor);
             preparedStatement.setBoolean(2, true);
             
-            //Executa a consulta SQL no banco de dados
+           
             result = preparedStatement.executeQuery();
-            //Itera por cada item do resultado
+            
             while (result.next()) {
-                //Se a lista não foi inicializada, a inicializa
-                if (listaQuartos == null) {
-                    listaQuartos = new ArrayList<Quarto>();
+                
+                if (listaProduto == null) {
+                    listaProduto = new ArrayList<Produto>();
                 }
-                //Cria uma instância de Quarto e popula com os valores do BD
-                Quarto quarto = new Quarto();
-                quarto.setId(result.getInt("quarto_id"));
-                quarto.setNumero(result.getLong("numero"));
-                quarto.setAndar(result.getLong("andar"));
-                quarto.setTipo(result.getString("tipo"));
-                quarto.setSituacao(result.getString("situacao"));
-                //Adiciona a instância na lista
-                listaQuartos.add(quarto);
+               
+                Produto produto = new Produto();
+                produto.setCod_produto(result.getLong("codproduto"));
+                produto.setModelo(result.getString("modelo"));
+                produto.setTamanho(result.getString("tamanho"));
+                produto.setMarca(result.getString("marca"));
+                produto.setCor(result.getString("cor"));
+                produto.setGenero(result.getString("genero"));
+                produto.setPreco(result.getFloat("preco"));
+               
+              
+                listaProduto.add(produto);
             }
         } finally {
-            //Se o result ainda estiver aberto, realiza seu fechamento
+            
             if (result != null && !result.isClosed()) {
                 result.close();
             }
-            //Se o preparedStatement ainda estiver aberto, realiza seu fechamento
+            
             if (preparedStatement != null && !preparedStatement.isClosed()) {
                 preparedStatement.close();
             }
-            //Se a conexão ainda estiver aberta, realiza seu fechamento
+            
             if (connection != null && !connection.isClosed()) {
                 connection.close();
             }
         }
-        //Retorna a lista de quartos do banco de dados
-        return listaQuartos;
+        
+        return listaProduto;
     }
+         public static Produto obter(Long CodProduto)
+            throws SQLException, Exception {
+        
+        String sql = "SELECT * FROM produto WHERE (codproduto=? AND enabled=?)";
+
+       
+        Connection connection = null;
+      
+        PreparedStatement preparedStatement = null;
+       
+        ResultSet result = null;
+        try {
+            
+            connection = ConnectionUtils.getConnection();
+           
+            preparedStatement = connection.prepareStatement(sql);
+          
+            preparedStatement.setLong(1, CodProduto);
+            preparedStatement.setBoolean(2, true);
+            
+            
+            result = preparedStatement.executeQuery();
+            
+             
+            if (result.next()) {                
+               
+                Produto produto = new Produto();
+                produto.setCod_produto(result.getLong("codproduto"));
+                produto.setModelo(result.getString("modelo"));
+                produto.setTamanho(result.getString("tamanho"));
+                produto.setMarca(result.getString("marca"));
+                produto.setCor(result.getString("cor"));
+                produto.setGenero(result.getString("genero"));
+                produto.setPreco(result.getFloat("preco"));
+                
+                
+                
+                return produto;
+            }            
+        } finally {
+            
+            if (result != null && !result.isClosed()) {
+                result.close();
+            }
+            
+            if (preparedStatement != null && !preparedStatement.isClosed()) {
+                preparedStatement.close();
+            }
+            
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }       
+
+        
+        return null;
+    }
+      
          
      }
     
